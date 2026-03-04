99c8dcb80549ca77182cf04a01b209876be9bc9f
Tiny IT <it@tiny.com>
2026-03-04 17:10:39 -0500

    Update CollectAutopilotHash.ps1

--- CollectAutopilotHash.ps1
+++ CollectAutopilotHash.ps1
@@ -1,17 +1,27 @@
-# Save as: CollectAutopilotHash.ps1
+# CollectAutopilotHash.ps1
+
+$secret = Read-Host "Enter access code"
 
 $serial = (Get-WmiObject Win32_BIOS).SerialNumber
-$csv = "C:\autopilot-$serial.csv"
+$hash   = (Get-WmiObject -Namespace root/cimv2/mdm/dmmap `
+           -Class MDM_DevDetail_Ext01 `
+           -Filter "InstanceID='Ext' AND ParentID='./DevDetail'").DeviceHardwareData
+$winPID = (Get-WmiObject Win32_OperatingSystem).SerialNumber
 
-# Self-contained hash collection - no internet required
-$devDetail = (Get-WmiObject -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
-$hash = $devDetail.DeviceHardwareData
+$csv = "Device Serial Number,Windows Product ID,Hardware Hash`n$serial,$winPID,$hash"
 
-$obj = [PSCustomObject]@{
-    "Device Serial Number" = $serial
-    "Windows Product ID"   = (Get-WmiObject Win32_OperatingSystem).SerialNumber
-    "Hardware Hash"        = $hash
-}
+$payload = @{
+    authToken = $secret
+    serial    = $serial
+    winPID    = $winPID
+    hash      = $hash
+    csv       = $csv
+} | ConvertTo-Json
 
-$obj | Export-Csv -Path $csv -NoTypeInformation
-Write-Host "Hash saved to $csv" -ForegroundColor Green
+Invoke-RestMethod `
+    -Uri "https://script.google.com/macros/s/AKfycbxcBqcB9drgN_yaf8nXKkxBBKJ1mqjDGcuPW1n0q30i32x0k7FiHpQj3DyGNTZ4TdZTXw/exec" `
+    -Method Post `
+    -Body $payload `
+    -ContentType "application/json"
+
+Write-Host "Hash submitted for device: $serial" -ForegroundColor Green
